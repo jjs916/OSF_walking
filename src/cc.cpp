eb7551610f3198f8fd3a9ca377a062b5adccfc4a
jjs916 <jjs916@snu.ac.kr>
2022-02-28 17:35:55 +0900

    init include

--- src/cc.cpp
+++ src/cc.cpp
@@ -342,20 +342,53 @@ void CustomController::computeSlow()
     }
     else if (rd_.tc_.mode == 11)
     {
-      WBC::SetContact(rd_, 1, 1);
-
-      if(initial_flag == 0)
+      if (rd_.tc_init)
       {
-        walking_enable_ = true;
-        initial_flag = 1;
-        walking_tick_mj = 0;
-        walking_end_flag = 0;
-        cout << "init\n" << endl;
+        std::cout << "mode 11 init" << std::endl;
+        rd_.tc_init = false;
+
+        rd_.link_[COM_id].x_desired = rd_.link_[COM_id].x_init;
       }
 
-      Gravity_torque = WBC::GravityCompensationTorque(rd_);
-      Total_torque = Gravity_torque;
-      rd_.torque_desired = Total_torque;
+      WBC::SetContact(rd_, 1, 1);
+
+      rd_.J_task.setZero(9, MODEL_DOF_VIRTUAL);
+      rd_.J_task.block(0, 0, 6, MODEL_DOF_VIRTUAL) = rd_.link_[COM_id].Jac();
+      rd_.J_task.block(6, 0, 3, MODEL_DOF_VIRTUAL) = rd_.link_[Upper_Body].Jac().block(3, 0, 3, MODEL_DOF_VIRTUAL);
+
+      rd_.link_[COM_id].x_desired = rd_.tc_.ratio * rd_.link_[Left_Foot].x_init + (1 - rd_.tc_.ratio) * rd_.link_[Right_Foot].x_init;
+      rd_.link_[COM_id].x_desired(2) = rd_.tc_.height;
+
+      rd_.link_[Upper_Body].rot_desired = DyrosMath::Euler2rot(rd_.tc_.roll, rd_.tc_.pitch, rd_.tc_.yaw + rd_.link_[Pelvis].yaw_init);
+
+      Eigen::VectorXd fstar;
+      rd_.link_[COM_id].SetTrajectoryQuintic(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
+      rd_.link_[Upper_Body].SetTrajectoryRotation(rd_.control_time_, rd_.tc_time_, rd_.tc_time_ + rd_.tc_.time);
+
+      fstar.setZero(9);
+      fstar.segment(0, 6) = WBC::GetFstar6d(rd_.link_[COM_id]);
+      fstar.segment(6, 3) = WBC::GetFstarRot(rd_.link_[Upper_Body]);
+
+      rd_.torque_desired = WBC::ContactForceRedistributionTorque(rd_, WBC::GravityCompensationTorque(rd_) + WBC::TaskControlTorque(rd_, fstar));
+
+      WBC::SetContact(rd_, 1, 1);
+
+      if (rd_.control_time_ >= rd_.tc_time_ + rd_.tc_.time)
+      {
+        if (initial_flag == 0)
+        {
+          walking_enable_ = true;
+          initial_flag = 1;
+          walking_tick_mj = 0;
+          walking_end_flag = 0;
+          cout << "init\n"
+               << endl;
+        }
+
+        Gravity_torque = WBC::GravityCompensationTorque(rd_);
+        Total_torque = Gravity_torque;
+        rd_.torque_desired = Total_torque;
+      }
     }
     else if (rd_.tc_.mode == 12){
       if (walking_enable_ == true)
@@ -582,12 +615,12 @@ void CustomController::copyRobotData(RobotData &rd_l)
 
 void CustomController::parameterSetting()
 {
-    target_x_ = 0.5;
+    target_x_ = 5.0;
     target_y_ = 0.0;
     target_z_ = 0.0;
     com_height_ = 0.71;
     target_theta_ = 0.0;
-    step_length_x_ = 0.10;
+    step_length_x_ = 0.20;
     step_length_y_ = 0.0;
     is_right_foot_swing_ = 1;
 
